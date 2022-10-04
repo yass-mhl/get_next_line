02fef1f74a480cc42257105fac35cecbfba4d471
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-04 21:02:55 +0200

    get_next_line V1

--- get_next_line.c
+++ get_next_line.c
@@ -1,33 +1,37 @@
 #include "get_next_line.h"
 
-static int *ft_line(char *stash, char **line)
+static int ft_line(char **stash, char **line)
 {
   int i;
   char *tmp;
 
   i = 0;
-  while(stash[i] != '\n' && stash[i] != 0)
+  while((*stash)[i] != '\n' && (*stash)[i] != 0)
     i++;
-  if(stash[i] == '\n')
+  if((*stash)[i] == '\n')
   {
-    *line = ft_strsub(stash, 0, i);
-    tmp = ft_strdup (stash + i + 1);
-    if (stash[0] == 0)
-      free(stash);
+    *line = ft_strsub((*stash), 0, i);
+    tmp = ft_strdup ((*stash) + i + 1);
+    free((*stash));
+    (*stash) = tmp;
+    if ((*stash)[0] == 0)
+    {
+      ft_strdel(stash);
+    }
   }
-  else if (stash[i] == 0)
+  else if ((*stash)[i] == 0)
   {
-    *line = ft_strsub(stash, 0, i);
-    free(stash);
+    *line = ft_strdup((*stash));
+    ft_strdel(stash);
   }
   return (1);
 }
 
-static int ft_return_value(char *stash, char **line, int n)
+static int ft_return_value(char **stash, char **line, int n)
 {
   if (n < 0)
     return (-1);
-  else if (n == 0 && stash == NULL)
+  else if (n == 0 && (*stash) == NULL)
     return (0);
   else
     return (ft_line(stash, line));
@@ -35,12 +39,12 @@ static int ft_return_value(char *stash, char **line, int n)
 
 int get_next_line(const int fd, char **line)
 {
-  char *buf[BUFF_SIZE + 1];
+  char buf[BUFF_SIZE + 1];
   static char *stash;
   char *tmp;
   int n;
 
-  while ((n = read(fd, buf, BUFF_SIZE) > 0))
+  while ((n = read(fd, buf, BUFF_SIZE)) > 0)
   {
     buf[n] = 0;
     if (stash == NULL)
@@ -54,7 +58,19 @@ int get_next_line(const int fd, char **line)
     if (ft_strchr(stash, '\n'))
       break;
   }
-  return (ft_return_value(stash, line, n));
+  return (ft_return_value(&stash, line, n));
 }
 
-#include <fcntl.h>
+int main(int ac, char **av)
+{
+  int fd;
+	char *line;
+
+	if (ac != 2)
+		printf("Error");
+	fd = open(av[1], O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
+		printf("%s\n", line);
+	close(fd);
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#include <fcntl.h>
 # include "libft/libft.h"
 #define BUFF_SIZE 20
 
