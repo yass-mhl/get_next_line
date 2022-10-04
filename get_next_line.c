d6b599aaa4a79a6f9e8c7eb7c039ee8377d657eb
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-04 18:26:58 +0200

    ft_line V1

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,28 @@
 #include "get_next_line.h"
 
+static int *ft_line(char *stash, char **line)
+{
+  int i;
+  char *tmp;
+
+  i = 0;
+  while(stash[i] != '\n' && stash[i] != 0)
+    i++;
+  if(stash[i] == '\n')
+  {
+    *line = ft_strsub(stash, 0, i);
+    tmp = ft_strdup (stash + i + 1);
+    if (stash[0] == 0)
+      free(stash);
+  }
+  else if (stash[i] == 0)
+  {
+    *line = ft_strsub(stash, 0, i);
+    free(stash);
+  }
+  return (1);
+}
+
 int get_next_line(const int fd, char **line)
 {
   char *buf[BUFF_SIZE + 1];
@@ -21,6 +44,7 @@ int get_next_line(const int fd, char **line)
     if (ft_strchr(stash, '\n'))
       break;
   }
+  return ;
 }
 
 #include <fcntl.h>
