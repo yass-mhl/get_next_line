00c13b243fe6e6f4cc587b6c3bcf018772641699
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-04 17:41:56 +0200

    Read until we find \n

--- get_next_line.c
+++ get_next_line.c
@@ -2,5 +2,25 @@
 
 int get_next_line(const int fd, char **line)
 {
+  char *buf[BUFF_SIZE + 1];
+  static char *stash;
+  char *tmp;
+  int n;
 
+  while ((n = read(fd, buf, BUFF_SIZE) > 0))
+  {
+    buf[n] = 0;
+    if (stash == NULL)
+      stash = ft_strdup(buf);
+    else
+    {
+      tmp = ft_strjoin(stash, buf);
+      free(stash);
+      stash = tmp;
+    }
+    if (ft_strchr(stash, '\n'))
+      break;
+  }
 }
+
+#include <fcntl.h>
