1fc388416480b2376fcd4b0f67b1463b4597a1a1
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-05 15:49:57 +0200

    recode gnl without internet

--- gnl.c
+++ gnl.c
@@ -1,84 +1,69 @@
 #include "get_next_line.h"
 
-char *ft_select(char *str, int *n)
+void ft_line(char **stash, char **line)
 {
   int i;
-  char *dest;
-
-  i = 0;
-  while (str[i] && str || str[i] != 0)
-    i++;
-  dest = ft_memalloc(i + 1);
-  if (ft_strchr(str, '\n') != 0)
-    *n = -1;
-  i = 0;
-  while (str[i] && str || str[i] != 0)
-  {
-    dest[i] = str[i];
-    i++;
-  }
-  dest[i] = 0;
-  return (dest);
-}
-
-char *ft_select2(char *str)
-{
-  int i;
-  int j;
-  char *dest;
-
-  i = 0;
-  while (str[i] && str || str[i] != 0)
-    i++;
-  dest = ft_memalloc(ft_strlen(str) - i + 1);
-  j = 0;
-  while(str[i] && str)
-    dest[j++] = str[++i];
-  dest[j] = 0;
-  return (dest);
-}
-
-int get_next_line(const int fd, char **line)
-{
-  char buf[BUFF_SIZE + 1];
-  int n;
-  static char *str;
   char *tmp;
 
-  n = BUFF_SIZE;
-  if (fd < 0 || BUFF_SIZE < 1)
-    return (-1);
-  while (n > 0)
+  i = 0;
+  while((*stash)[i] && (*stash)[i] != '\n')
+    i++;
+  if ((*stash)[i] == '\n')
   {
-    n = read(fd, buf, BUFF_SIZE);
-    buf[BUFF_SIZE] = 0;
-    if (n == -1)
-      return (-1);
-    tmp = str;
-    str = ft_strjoin(tmp, buf);
-    free(tmp);
-    if (ft_strchr(str, '\n'))
-      break ;
+    *line = ft_strsub((*stash), 0, i);
+    tmp = ft_strdup((*stash) + i + 1 );
+    free((*stash));
+    (*stash) = tmp;
+    if ((*stash)[0] == 0)
+      ft_strdel(stash);
   }
-  *line = ft_select(str, &n);
-  tmp = str;
-  str = ft_select2(tmp);
-  free(tmp);
-  if (n == 0 && (*str)[0] == 0)
-    ft_strdel(&str);
-  return (n);
+  else if((*stash)[i] == 0)
+  {
+    *line = ft_strdup(*stash);
+    ft_strdel(stash);
+  }
 }
 
-int main(int ac, char **av)
+int get_next_line(int fd, char **line)
+{
+  int n;
+  static char *stash;
+  char *tmp;
+  char buf[BUFF_SIZE + 1];
+
+  n = BUFF_SIZE;
+  if (fd < 0 || BUFF_SIZE < 1 || !line)
+    return (-1);
+  while(n > 0)
+  {
+    n = read(fd, buf, BUFF_SIZE);
+    buf[n] = 0;
+    if (stash == NULL)
+      stash = ft_strdup(buf);
+    else
+    {
+      tmp = ft_strjoin(stash, buf);
+      free(stash);
+      stash = tmp;
+    }
+    if (ft_strchr(stash, '\n'))
+      break ;
+  }
+  ft_line(&stash, line);
+   if (n < 0)
+    return (-1);
+  else if (n == 0 && stash == NULL)
+    return (0);
+  else
+    return (1);
+}
+
+int main()
 {
   int fd;
 	char *line;
 
-	if (ac != 2)
-		printf("Error");
-	fd = open(av[1], O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
-		printf("%s\n", line);
-	close(fd);
-	return (0);
+	fd = open("test.txt", O_RDONLY);
+  while(get_next_line(fd, &line) > 0)
+    printf("%s\n", line);
 }
